9d39ebcc7d727bffeed392a7736b562eed5961e3
Francesco De Carlo <decarlofra@B116-06Wn.GALILEI>
2025-04-01 12:13:15 +0200

    add parola casuale

--- script/logica.js
+++ script/logica.js
@@ -1,76 +1,74 @@
 function turno() {
-    console.log("turno " + turnoCorrente)
-    if (turnoCorrente == 0) {
-        //quello che accade finche' non si schiaccia per la prima volta il bottone prossimo turno (o si preme invio)
-        aggiornaCaselle()
-        turnoCorrente = 1
-    } else {
-        //flow normale del gioco
-        let parola = risolviParola()
-        if (parola) {
-            let vinto = controllaParola(parola)
+	console.log("turno " + turnoCorrente)
+	if (turnoCorrente == 0) {
+		//quello che accade finche' non si schiaccia per la prima volta il bottone prossimo turno (o si preme invio)
+		aggiornaCaselle()
+		turnoCorrente = 1
+	} else {
+		//flow normale del gioco
+		let parola = risolviParola()
+		if (parola) {
+			let vinto = controllaParola(parola)
 
-            if (vinto) {
-                rimuoviEventListenerTastiera() //per evitare di passare al turno successivo schiaggiando enter durante l'alert
-                setTimeout(() => {
-                    alert("Hai indovinato la parola!")
-                    iniziaGioco(numeroLettere)
-                })
-            }
-            if (turnoCorrente >= MAX_TURNI) {
-                rimuoviEventListenerTastiera()
-                setTimeout(() => {
-                    alert("Hai esaurito i turni!")
-                    iniziaGioco(numeroLettere)
-                })
-            }
+			if (vinto) {
+				rimuoviEventListenerTastiera() //per evitare di passare al turno successivo schiaggiando enter durante l'alert
+				setTimeout(() => {
+					alert("Hai indovinato la parola!")
+					iniziaGioco(numeroLettere)
+				})
+			} else if (turnoCorrente >= MAX_TURNI) {
+				rimuoviEventListenerTastiera()
+				setTimeout(() => {
+					alert("Hai esaurito i turni!")
+					iniziaGioco(numeroLettere)
+				})
+			}
 
-            aggiornaCaselle()
-            turnoCorrente++
-        } else {
-            alert("Parola non valida!")
-        }
-    }
+			aggiornaCaselle()
+			turnoCorrente++
+		} else {
+			alert("Parola non valida!")
+		}
+	}
 }
 
 //prende il valore delle caselle e lo restituisce come stringa
 function risolviParola() {
-    let ris = ""
-    let turnoPrecedente = turnoCorrente - 1
-    for (let i = 0; i < numeroLettere; i++) {
-        let cella = document.querySelector("#campo" + turnoPrecedente + "-" + i)
-        if (
-            !cella.value ||
-            cella.value == " " ||
-            cella.value.length != 1 ||
-            CARATTERI_SPECIALI.includes(cella.value)
-        ) {
-            ris = false
-        } else {
-            ris += cella.value
-        }
-    }
-    console.log(ris)
+	let ris = ""
+	let turnoPrecedente = turnoCorrente - 1
+	for (let i = 0; i < numeroLettere; i++) {
+		let cella = document.querySelector("#campo" + turnoPrecedente + "-" + i)
+		if (
+			!cella.value ||
+			cella.value == " " ||
+			cella.value.length != 1 ||
+			CARATTERI_SPECIALI.includes(cella.value)
+		) {
+			ris = false
+		} else {
+			ris += cella.value
+		}
+	}
+	console.log(ris)
 
-    return ris
+	return ris
 }
 
-const PAROLA_CORRETTA = "casa" //temporanea
 function controllaParola(parola) {
-    let turnoPrecedente = turnoCorrente - 1
-    let caratteriCorretti = 0
-    for (let i = 0; i < parola.length; i++) {
-        let carattere = parola[i]
-        if (carattere == PAROLA_CORRETTA[i]) {
-            caratteriCorretti++
-            document.querySelector(
-                "#cella" + turnoPrecedente + "-" + i
-            ).style.backgroundColor = "green"
-        } else if (PAROLA_CORRETTA.includes(carattere)) {
-            document.querySelector(
-                "#cella" + turnoPrecedente + "-" + i
-            ).style.backgroundColor = "yellow"
-        }
-    }
-    return caratteriCorretti == parola.length
+	let turnoPrecedente = turnoCorrente - 1
+	let caratteriCorretti = 0
+	for (let i = 0; i < parola.length; i++) {
+		let carattere = parola[i]
+		if (carattere == parolaCorretta[i]) {
+			caratteriCorretti++
+			document.querySelector(
+				"#cella" + turnoPrecedente + "-" + i
+			).style.backgroundColor = "green"
+		} else if (parolaCorretta.includes(carattere)) {
+			document.querySelector(
+				"#cella" + turnoPrecedente + "-" + i
+			).style.backgroundColor = "yellow"
+		}
+	}
+	return caratteriCorretti == parola.length
 }
--- script/script.js
+++ script/script.js
@@ -6,49 +6,71 @@ const CARATTERI = "abcdefghijklmnopqrstuvwxyzàèéìòù"
 
 const MAX_TURNI = 5
 
+var parolaCorretta = ""
 var numeroLettere = 5
 var turnoCorrente = 0
 //
 //
 //
 //funzione principale
-function iniziaGioco(n) {
-    turnoCorrente = 0
-    if (n >= 4 && n <= 10) {
-        numeroLettere = n
-    } else {
-        numeroLettere = 5
-    }
-    creaPagina()
-    aggiungiEventListenerTastiera()
+async function iniziaGioco(n) {
+	parolaCorretta = await scegliParolaCasuale(n)
+	console.log(parolaCorretta)
 
-    turno() //primo turno
+	turnoCorrente = 0
+	if (n >= 4 && n <= 10) {
+		numeroLettere = n
+	} else {
+		numeroLettere = 5
+	}
+	creaPagina()
+	aggiungiEventListenerTastiera()
+
+	turno() //primo turno
 }
 
-async function caricaFile(nomeFile) {
-    try {
-        const risposta = await fetch(nomeFile)
+async function caricaParole(nomeFile) {
+	try {
+		const risposta = await fetch(nomeFile)
 
-        if (!risposta.ok) {
-            throw new Error("Errore nel caricamento del file")
-        }
-        console.log("Caricamento avvenuto con successo")
-    } catch (error) {
-        console.error("Errore durante il caricamento del file")
-        return null
-    }
+		if (!risposta.ok) {
+			throw new Error("Errore nel caricamento del file")
+		}
+		console.log("Caricamento avvenuto con successo")
+		return await risposta.json()
+	} catch (error) {
+		console.error("Errore durante il caricamento del file")
+		return null
+	}
+}
+
+async function scegliParolaCasuale(n) {
+	let chiave = n + "lettere"
+	let parole = (await caricaParole("../parole.json"))[chiave]
+	return parole[Math.floor(Math.random() * parole.length)]
+}
+
+function scegliIndiciCasuali(qta, parola) {
+	let posizioniScelte = []
+	for (let i = 0; i < qta; i++) {
+		do {
+			let posizioneCasuale = Math.floor(Math.random() * parola.length)
+		} while (!posizioniScelte.includes(posizioneCasuale))
+		posizioniScelte.push(posizioneCasuale)
+	}
+	return posizioniScelte
 }
 
 function vaiAllaPagina() {
-    let valore = document.querySelector("#numero").value
-    window.location.href = "gioco.html?numero=" + valore
+	let valore = document.querySelector("#numero").value
+	window.location.href = "gioco.html?numero=" + valore
 }
 
 function estraiNumero() {
-    let parametri = new URLSearchParams(window.location.search)
-    let numero = parseInt(parametri.get("numero"))
-    if (isNaN(numero)) {
-        numero = 5
-    }
-    return numero
+	let parametri = new URLSearchParams(window.location.search)
+	let numero = parseInt(parametri.get("numero"))
+	if (isNaN(numero)) {
+		numero = 5
+	}
+	return numero
 }
